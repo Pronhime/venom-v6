86c44c3ad2dfaf020e9515ae5927c08f7402c042
Pronhime <protu212@gmail.com>
2024-11-24 08:46:19 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7799606819:AAG_xB27Hs3lmiBF7qfB6y5OL995smqLfow'
+ADMIN_ID = '1773970205'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
